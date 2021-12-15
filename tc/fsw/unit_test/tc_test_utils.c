902c3a5705182f3c97f6ec25600703f16bc56693
Rob Stoddard <robert.n.stoddard@gmail.com>
2021-12-14 20:04:21 -0500

    added table values into the test env

--- tc/fsw/unit_test/tc_test_utils.c
+++ tc/fsw/unit_test/tc_test_utils.c
@@ -24,9 +24,26 @@
 extern TC_AppData_t  g_TC_AppData;
 WISE_HkTlm_t  wise_tlm;
 
+//Test Table Definition due to build issue loading the table binary file
+TC_ILoadTblEntry_t TC_Test_ILoadDefTbl[TC_ILOAD_MAX_ENTRIES] =
+{
+    /* Entry 1 */
+    {
+        .NeedsCooling_Active = 25,
+        .CoolEnough_Active = 20,
+        .HotEnough_Active = 20,
+        .NeedsHeating_Active = 15,
+        .NeedsCooling_Inactive = 40,
+        .CoolEnough_Inactive = 20,
+        .HotEnough_Inactive = 20,
+        .NeedsHeating_Inactive = 5
+    }
+};
+
 /*
  * Function Definitions
  */
+
 
 void TC_Test_Setup(void)
 {
@@ -34,6 +51,9 @@ void TC_Test_Setup(void)
 
     CFE_PSP_MemSet(&g_TC_AppData, 0, sizeof(TC_AppData_t));
     
+    g_TC_AppData.ILoadTblPtr = &TC_Test_ILoadDefTbl[0];
+    //CFE_PSP_MemSet(g_TC_AppData.ILoadTblPtr, 0, sizeof(TC_ILoadTblEntry_t)*TC_ILOAD_MAX_ENTRIES);
+
     Ut_CFE_EVS_Reset();
     Ut_CFE_FS_Reset();
     Ut_CFE_TIME_Reset();
@@ -47,6 +67,20 @@ void TC_Test_Setup(void)
     memset((void*)&wise_tlm, 0x00, sizeof(wise_tlm));
     CFE_SB_InitMsg(&wise_tlm,
                    WISE_HK_TLM_MID, sizeof(wise_tlm), TRUE);
+
+    TC_ProcessNewILoadTbl();
+
+    //init the table:
+    UtPrintf("DEBUG: Threshold Temp Table Values: (active): %i,%i,%i,%i (inactive): %i,%i,%i,%i\n",
+    g_TC_AppData.ThresholdTemps.NeedsCooling_Active,
+    g_TC_AppData.ThresholdTemps.CoolEnough_Active,
+    g_TC_AppData.ThresholdTemps.HotEnough_Active,
+    g_TC_AppData.ThresholdTemps.NeedsHeating_Active,
+    g_TC_AppData.ThresholdTemps.NeedsCooling_Inactive,
+    g_TC_AppData.ThresholdTemps.CoolEnough_Inactive,
+    g_TC_AppData.ThresholdTemps.HotEnough_Inactive,
+    g_TC_AppData.ThresholdTemps.NeedsHeating_Inactive 
+    );
 
 } /* end Example_Test_Setup */
 
